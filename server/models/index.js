3a48fee63e271f9139b18d5e9880fb6a08207c1e
maxwell walin <maxwellwalin@gmail.com>
2021-11-19 17:40:57 -0800

    SERVER: moved server auth util to client side, created server side util, created index.js in models to export models to server.js; CLIENT: created mutations and queries utils bases

---
+++ server/models/index.js
@@ -0,0 +1,8 @@
+const User = require('./User');
+const Part = require('./Part');
+const Category = require('./Category');
+const Make = require('./Make');
+const Model = require('./Model');
+const Year = require('./Year');
+
+module.exports = { User, Part, Category, Make, Model, Year };
