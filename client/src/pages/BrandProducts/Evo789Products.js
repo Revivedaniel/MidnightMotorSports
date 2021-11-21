b5fb03e0438fd7200258abd8fd23d3dd91948892
maxwell walin <maxwellwalin@gmail.com>
2021-11-21 14:28:09 -0800

    added working routes for mitsubishi models

---
+++ client/src/pages/BrandProducts/Evo789Products.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Evo9 from '../../images/transparentevo9.png'
+
+export default function Evo789() {
+    return (
+        <div>
+            <img src={Evo9} alt='evoix image' className='evo9img' />
+        </div>
+    )
+}
---
+++ client/src/pages/BrandProducts/EvoXProducts.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Evox from '../../images/transparentevox.png'
+
+export default function EvoX() {
+    return (
+        <div>
+            <img src={Evox} alt='evox image' className='evoximg' />
+        </div>
+    )
+}
