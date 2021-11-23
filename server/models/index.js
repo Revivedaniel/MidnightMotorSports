5d78e1ff3700f0e2f3d9d568c4bfe8338fd286e5
Revivedaniel <mlg.danielstark@gmail.com>
2021-11-22 19:52:08 -0800

    Deleting year from index

--- server/models/index.js
+++ server/models/index.js
@@ -3,6 +3,5 @@ const Part = require('./Part');
 const Category = require('./Category');
 const Make = require('./Make');
 const Model = require('./Model');
-const Year = require('./Year');
 
-module.exports = { User, Part, Category, Make, Model, Year };
+module.exports = { User, Part, Category, Make, Model };
