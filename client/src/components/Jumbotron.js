57bb45e4e3f242ec66a83f2b3fa807353caeda0f
Revivedaniel <mlg.danielstark@gmail.com>
2021-11-28 21:43:19 -0800

    Carousel is slightly responsive

--- client/src/components/Jumbotron.js
+++ client/src/components/Jumbotron.js
@@ -3,7 +3,7 @@
 // if on products page, carousel of each car brand that the shop has performed work on (BMW, Subaru, Mitsubishi) that are CLICKABLE, takes you to that brands product page
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Whitebmw from '../images/whitem3.jpg'
-import Brakeswap from '../images/civicbrakeswap.png'
+import BlackR35 from '../images/blackr35.jpg'
 import R35swap from '../images/r35swap.jpg'
 import Midnightdoor from '../images/midnightdoor.jpg'
 import { Carousel } from 'react-bootstrap'
@@ -17,7 +17,7 @@ export default class Jumbotron extends Component {
                 <Carousel>
   <Carousel.Item interval={1000}>
     <img
-      className="d-block w-100 carouselImg"
+      className="d-block carouselImg"
       src={R35swap}
       alt="First slide"
       width='500px'
@@ -30,7 +30,7 @@ export default class Jumbotron extends Component {
   </Carousel.Item>
   <Carousel.Item interval={500}>
     <img
-      className="d-block w-100 carouselImg"
+      className="d-block carouselImg"
       src={Whitebmw}
       alt="Second slide"
       width='500px'
@@ -42,8 +42,8 @@ export default class Jumbotron extends Component {
   </Carousel.Item>
   <Carousel.Item>
     <img
-      className="d-block w-100 carouselImg"
-      src={Brakeswap}
+      className="d-block carouselImg"
+      src={BlackR35}
       alt="Third slide"
       width='500px'
       height="600px"
