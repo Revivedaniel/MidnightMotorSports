7f1a37b968197bd029f1c4a380543c977ecf54fa
maxwell walin <maxwellwalin@gmail.com>
2021-12-16 13:29:01 -0800

    debugged icons not populating on products page, added styling to mitsubishi models

--- client/src/pages/Brands/Mitsubishi.js
+++ client/src/pages/Brands/Mitsubishi.js
@@ -10,25 +10,19 @@ export default function Mitsubishi() {
     const Xanime = useSpring({ from: { x: 800, opacity: 0 }, to: { x: 0, opacity: 1 }, delay: 200, config: config.molasses })
 
     return (
-        <div>
-            <div className='mitsuimgs'>
-                <div>
-                    <Link to="/Mitsubishi/evo789" style={{ textDecoration: 'none' }}>
-                        <animated.div style={evo7anime}>
-                            <img src={Evo9} alt='evoix image' className='evo9img' />
-                        </animated.div>
-                        <h1 className='carmodelname'>EVO7/8/9</h1>
-                    </Link>
-                </div>
-                <div>
-                    <Link to="Mitsubishi/evox" style={{ textDecoration: 'none' }}>
-                        <animated.div style={Xanime}>
-                            <img src={Evox} alt='evox image' className='evoximg' />
-                        </animated.div>
-                        <h1 className='carmodelname'>EVOX</h1>
-                    </Link>
-                </div>
-            </div>
+        <div className='mitsuimgs'>
+            <Link to="/Mitsubishi/evo789">
+                <animated.div style={evo7anime}>
+                    <img src={Evo9} className='evo9img' alt='grey mitsubishi evo7/8/9' />
+                </animated.div>
+                <h1 className='carmodelname'>EVO7/8/9</h1>
+            </Link>
+            <Link to="Mitsubishi/evox">
+                <animated.div style={Xanime}>
+                    <img src={Evox} className='evoximg' alt='red mitsubishi evox' />
+                </animated.div>
+                <h1 className='carmodelname'>EVOX</h1>
+            </Link>
         </div>
     )
 }
